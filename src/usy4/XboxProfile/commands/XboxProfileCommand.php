0c45dc9171140ff850428969be722963676628d9
usy4 <68358524+usy4@users.noreply.github.com>
2022-12-20 21:10:27 +0300

    Update XboxProfileCommand.php

--- src/usy4/XboxProfile/commands/XboxProfileCommand.php
+++ src/usy4/XboxProfile/commands/XboxProfileCommand.php
@@ -20,11 +20,11 @@ namespace usy4\XboxProfile\commands;
  * 	
  */
 
-use CortexPE\Commando\args\TargetPlayerArgument;
 use CortexPE\Commando\BaseCommand;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 
+use usy4\XboxProfile\commands\TargetPlayerArgument;
 use usy4\XboxProfile\Main;
 
 class XboxProfileCommand extends BaseCommand 
