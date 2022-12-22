c555dfd9b97362efb30fb72b6f879646b37d99e4
usy4 <68358524+usy4@users.noreply.github.com>
2022-12-22 17:13:38 +0300

    Update Main.php

--- src/usy4/XboxProfile/Main.php
+++ src/usy4/XboxProfile/Main.php
@@ -3,6 +3,7 @@
 namespace usy4\XboxProfile;
 
 use usy4\XboxProfile\commands\XboxProfileCommand;
+use usy4\XboxProfile\tasks\GetInfoTask;
 
 use CortexPE\Commando\PacketHooker;
 
@@ -22,36 +23,32 @@ class Main extends PluginBase{
 
     public static function xboxProfileForm(Player $player, string $gt) : void {
         $gt_nospace = str_replace(' ', '%20', $gt);
-        $url = "https://playerdb.co/api/player/xbox/" . $gt_nospace; // powerd by steam from 2016 to 2022
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        $result = curl_exec($ch);
-  
-        $obj = json_decode($result, false);
-
-        if($obj->code !== "player.found"){
-            $player->sendMessage("§cError, There is no xbox gamertag called: " . $gt);
-            return;
-        }
-        
-        $xProfile = (
-            "§dUserName: §f" . $obj->data->player->username . "\n" .
-            "§dGamerScore: §f" . $obj->data->player->meta->gamerscore . "\n" .
-            "§dAccountTier: §f" . $obj->data->player->meta->accountTier . "\n" .    
-            "§dXboxOneRep: §f" . $obj->data->player->meta->xboxOneRep . "\n" .
-            "§dRealName: §f" . $obj->data->player->meta->realName . "\n" .
-            "§dBio: §f" . $obj->data->player->meta->bio . "\n"
-        );
-        
-        $form = new SimpleForm(function (Player $player, $data){
-            if($data === null) {
-                return true;
+        Server::getInstance()->getAsyncPool()->submitTask(new GetInfoTask($gt_nospace, function (int $results, array $data, ?string $error = null) use ($player, $gt){      
+            if($data["code"] !== "player.found"){
+                $player->sendMessage("§cError, There is no xbox gamertag called: " . $gt);
+                return;
             }
-        });
-        $form->setTitle("§aXbox Profile");
-        $form->setContent($xProfile);
-        $form->addButton("§2:O");
-        $player->sendForm($form);
+            
+            $xProfile = (
+                "§aUserName: §f" . $data["data"]["player"]["username"] . "\n" .
+                "§aId: §f" . $data["data"]["player"]["id"] . "\n" .
+                "§aGamerScore: §f" . $data["data"]["player"]["meta"]["gamerscore"] . "\n" .
+                "§aAccountTier: §f" . $data["data"]["player"]["meta"]["accountTier"] . "\n" .    
+                "§aXboxOneRep: §f" . $data["data"]["player"]["meta"]["xboxOneRep"] . "\n" .
+                "§aRealName: §f" . $data["data"]["player"]["meta"]["realName"] . "\n" .
+                "§aBio: §f" . $data["data"]["player"]["meta"]["bio"] . "\n"
+            );
+
+            $form = new SimpleForm(function (Player $player, $data){
+                if($data === null) {
+                    return true;
+                }
+            });
+            $form->setTitle("§2Xbox Profile");
+            $form->setContent($xProfile);
+            $form->addButton("§2:O");
+            $player->sendForm($form);
+        }));
     }
+    
 }
